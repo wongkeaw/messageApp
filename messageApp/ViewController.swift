2fcd3c8541e20e63e9e7a015b75357783df58a92
ongarj wongkeaw <oa.wongkeaw@gmail.com>
2018-08-23 17:00:58 +0700

    Initial Commit

---
+++ messageApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  messageApp
+//
+//  Created by ongarj wongkeaw on 8/23/18.
+//  Copyright © 2018 ongarj wongkeaw. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
